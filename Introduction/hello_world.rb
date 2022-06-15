6450234ffc8e7106d0c885ac90e8f08ba3a9c939
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-06-15 18:39:27 -0300

    Add first Hello World app
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

---
+++ Introduction/hello_world.rb
@@ -0,0 +1,16 @@
+# Say hello world in multiple ways
+# Simple put string
+# Using a variab,e
+# Using a method
+
+puts "Hello World"
+
+greeting = "Hello World!\n"
+
+print greeting
+
+def say_hello(thing_to_say)
+  p thing_to_say
+end
+
+say_hello("Hello")
